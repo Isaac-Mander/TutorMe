2a6b11073aefab8646f580346e653c034779bc73
Hellolightn3ss <68981667+Hellolightn3ss@users.noreply.github.com>
2023-06-23 13:42:14 +1200

    contact page

---
+++ contact.php
@@ -0,0 +1,101 @@
+
+<?php
+
+session_start();
+if(!isset($_SESSION['user']) && !isset($_SESSION['school_code']) && !isset($_SESSION['user_id'])) //If not logged in redirect to login page
+{
+    header("Location: login_form.php"); //Send to the shadow realm (login screen)
+}
+
+//Get relevant info from session
+$user_id = $_SESSION['user_id'];
+
+//Import functions
+include("sys_page/header.html");
+include("sys_page/db_connect.php");
+include("sys_page/functions.php");
+?>
+
+
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link rel="stylesheet" href="sys_page/styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  </head>
+  <body>
+    <div id="copy_email_text">    
+    <p class="card">
+冷暖哪可休
+回头多少个秋
+寻遍了却偏失去
+未盼却在手
+我得到没有
+没法解释得失错漏
+刚刚听到望到便更改
+不知哪里追究
+一生何求
+常判决放弃与拥有
+耗尽我这一生
+触不到已跑开
+一生何求
+迷惘里永远看不透
+没料到我所失的
+竟已是我的所有
+冷暖哪可休
+回头多少个秋
+寻遍了却偏失去
+未盼却在手
+我得到没有
+没法解释得失错漏
+刚刚听到望到便更改
+不知哪里追究
+一生何求
+常判决放弃与拥有
+耗尽我这一生
+触不到已跑开
+一生何求
+迷惘里永远看不透
+没料到我所失的
+竟已是我的所有
+一生何求
+曾妥协也试过苦斗
+梦内每点缤纷
+一消散哪可收
+一生何求
+谁计较赞美与诅咒
+没料到我所失的
+竟已是我的所有
+一生何求
+常判决放弃与拥有
+耗尽我这一生
+触不到已跑开
+一生何求
+迷惘里永远看不透
+没料到我所失的
+竟已是我的所有</p></div>
+    <button class="btn  btn-primary" onclick="copy_text('copy_email_text')" id="button" type="button">button</button>
+    <script>
+    function copy_text(id)
+    {
+    var r = document.createRange();
+    r.selectNode(document.getElementById(id));
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(r);
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges();
+       // Copy the text inside the text field
+
+    // Alert the copied text
+    alert("Copied the text: " + r);
+    }
+    </script>
+    <script src="content.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+  </body>
+</html>

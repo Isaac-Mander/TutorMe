c01c19550c673accb0e286eb824098eb5d59f6cd
Isaac Mander <ima6796@stacmail.net>
2023-06-29 12:07:10 +1200

    Removed dev feature

--- sessions.php
+++ sessions.php
@@ -38,7 +38,6 @@ $session_today_tutee_data = get_session_data($session_today_tutee_sql,$conn);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
   </head>
   <body>
-  <button id="button">Click here</button>
   <?php
 
 if (is_array($session_today_tutor_data) && is_array($session_today_tutee_data)) {

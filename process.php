71ca7e8898ee9374545192439cad11cc56391d15
Hellolightn3ss <68981667+Hellolightn3ss@users.noreply.github.com>
2023-07-14 21:49:36 +1200

    codezilla

---
+++ process.php
@@ -0,0 +1,8 @@
+<?php
+include("sys_page/db_connect.php");
+include("sys_page/functions.php");
+
+//Gets the data sent from the form from calendar_1.php
+$sorting = $_POST['sorting'];
+header("Location: session_matching.php?sorting=" . $sorting);
+?>

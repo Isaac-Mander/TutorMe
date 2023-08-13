3f21d3c06672567146ed167f422c1d800083bd1a
Hellolightn3ss <68981667+Hellolightn3ss@users.noreply.github.com>
2023-08-13 21:36:16 +1200

    half way there

--- info_setting.php
+++ info_setting.php
@@ -318,29 +318,33 @@ $tz = new DateTimeZone('NZ');
     
     <h1><p class="text-center"><?php echo $display_name."'s profile";?></p></h1>
 
-    <div class="row">
-      <div class ="col" >
-        <div class="card"><h1>need help with</h1></div>
-         <div id="tutoring_subjects_checkbox_studying" class="hide_on_start">
-                <?php 
-              for($i=0;$i<sizeof($all_available_subject_array);$i++){ //Check if subject should be ticked on start
-                ?><div class="card mx-auto h-300" style="width: 25rem;"> <div class="card-body"> <?php
-                $checkbox_id = "checkbox_" . $checkbox_id_increment;//Set what the id of the checkbox should be
-                if($no_tutee_subjects) $all_available_subject_array[$i][3] = false; //If there are no subjects make sure the checkbox it unticked
-                if($all_available_subject_array[$i][3] == true) 
-                {
-                  ?>
-                  <div class="card-title"> <?php echo $all_available_subject_array[$i][1];?></div>
-                  <div class="card-footer"> <?php echo "<input id=" . $checkbox_id . " type='checkbox' checked>" ?> </div> <?php
-                } //Create a checked checkbox
-                else {
-                  ?>
-                  <div class="card-title"> <?php echo $all_available_subject_array[$i][1];?></div>
-                  <div class="card-footer"> <?php echo "<input id=" . $checkbox_id . " type='checkbox'>" ?> </div> <?php
-                } //Create an empty checkbox 
-                ?></div></div><?php
-                $checkbox_id_increment += 1; //Increment the checkbox id by 1
-                }?>
+<div class="row">
+  <div class ="col" >
+    <div class="card"><h1>need help with</h1></div>
+     <div id="tutoring_subjects_checkbox_studying" class="hide_on_start">
+      <div class="row">
+            <?php 
+          for($i=0;$i<sizeof($all_available_subject_array);$i++){ //Check if subject should be ticked on start
+            ?><div class="col-sm"><div class="card mx-auto" style="width: 25rem;"><div class="card-body"> <?php
+            $checkbox_id = "checkbox_" . $checkbox_id_increment;//Set what the id of the checkbox should be
+            if($no_tutee_subjects) $all_available_subject_array[$i][3] = false; //If there are no subjects make sure the checkbox it unticked
+            if($all_available_subject_array[$i][3] == true) 
+            {
+              echo "<img class='subject_icon card-image-top' src='sys_img\subject_icon.jpg' alt=''>";
+              ?>
+              <div class="card-text"> <?php echo $all_available_subject_array[$i][1];?></div>
+              <div class="card-footer"> <?php echo "<input id=" . $checkbox_id . " type='checkbox' checked>" ?> </div> <?php
+            } //Create a checked checkbox
+            else {
+              echo "<img class='subject_icon card-image-top' src='sys_img\subject_icon.jpg' alt=''>";
+              ?>
+              <div class="card-text"> <?php echo $all_available_subject_array[$i][1];?></div>
+              <div class="card-footer"> <?php echo "<input id=" . $checkbox_id . " type='checkbox'>" ?> </div> <?php
+             } //Create an empty checkbox 
+            ?></div></div></div><?php
+            $checkbox_id_increment += 1; //Increment the checkbox id by 1
+            }?>
+        </div>
         </div>
         <div id="studying_subject_cards">
               <?php
@@ -349,14 +353,14 @@ $tz = new DateTimeZone('NZ');
             {
               for($x=0;$x<sizeof($subject_array_tutee);$x++)
               {
-                  ?><div class="card mx-auto" style="width: 25rem;"><div class="card-body"><?php
+                  ?><div class="card mx-auto" style="width: 25rem; height: 5rem;"><div class="card-body"><?php
                   echo "<div ";
                   echo "id=tutee_" . $subject_element_tutee_id;//Give the element a unqiue id
                   echo " class='col'><p class='nowrap'>";
                   echo $subject_array_tutee[$x][2];
                   echo "";
                   echo "</p></div>";
-                  //<img class='hide_on_start edit_cross' src='sys_img/icons8-x-100.png' alt=''>
+                  //"<img class='hide_on_start edit_cross' src='sys_img/icons8-x-100.png' alt=''>"
                   //Increment the id ?></div></div><?php
                   $subject_element_tutee_id += 1;
               }
@@ -368,28 +372,31 @@ $tz = new DateTimeZone('NZ');
     <div class="col" id="tutoring">
     <div class="card"><h1>tutoring</h1></div>
       <div id="tutoring_subjects_checkbox_tutoring" class="hide_on_start">
+      <div class="row">
           <?php for($i=0;$i<sizeof($all_available_subject_array);$i++){ //Check if subject should be ticked on start
-          ?> <div class="card mx-auto" style="width: 25rem;"> <div class="card-body"> <div class="col"><p class="nowrap"> <?php
+          ?> <div class="col-sm" ><div class="card mx-auto" style="width: 25rem;"> <div class="card-body"><?php
             $checkbox_id = "checkbox_" . $checkbox_id_increment;//Set what the id of the checkbox should be
             if($no_tutor_subjects) $all_available_subject_array[$i][4] = false; //If there are no subjects make sure the checkbox it unticked
             if($all_available_subject_array[$i][4] == true) 
-            {
+            {              
+              echo "<img class='subject_icon card-image-top' src='sys_img\subject_icon.jpg' alt=''>";
               ?>
               <div class="card-text"> <?php echo $all_available_subject_array[$i][1];?></div>
               <div class="card-footer"> <?php echo "<input id=" . $checkbox_id . " type='checkbox' checked>" ?> </div> <?php
             } //Create a checked checkbox
             else 
             {
+              echo "<img class='subject_icon card-image-top' src='sys_img\subject_icon.jpg' alt=''>";
               ?>
               <div class="card-text"> <?php echo $all_available_subject_array[$i][1];?></div>
               <div class="card-footer"> <?php echo "<input id=" . $checkbox_id . " type='checkbox'>" ?> </div> <?php
               } //Create an empty checkbox 
-            ?></p></div></div></div>
+            ?></div></div></div>
             <?php
             $checkbox_id_increment += 1; //Increment the checkbox id by 1
             }?>
       </div>
-
+      </div>
       <div id="tutoring_subject_cards">
                 <?php
               //Get number of items in subject array
@@ -415,125 +422,167 @@ $tz = new DateTimeZone('NZ');
   </div>
             
     <button class="btn btn-success btn-md" id=profile_edit_button>Edit</button>
-    <div class="card mx-auto w-75">
-    <form action='calendar_2.php' method='post'>
-    <div class="card-body">
-    <div class="form-group row">
-        <label for="start_time" class="col-sm-2 col-form-label"><b>Start time</b></label>
-        <div class="col-sm-10">
-        <input type="time" class="form-control" id="start_time" placeholder="Start time" name="start_time" required><br>
-        </div>
-        </div>
-
-        <div class="form-group row">
-        <label for="end_time" class="col-sm-2 col-form-label"><b>End time</b></label>
-        <div class="col-sm-10">
-        <input type="time" class="form-control" id="end_time" placeholder="End time" name="end_time" required><br>
-        </div>
-        </div>
-
-        <fieldset class="form-group">
-          <div class="row">
-            <legend class="col-form-label col-sm-2 pt-0">Day of week</legend>
-            <div class="col-sm-10">
-              <div class="form-check">
-                <input class="form-check-input" type="radio" name="day_of_week" id="day_of_week1" value=1>
-                <label class="form-check-label" for="gridRadios1">
-                  Monday
-                </label>
-              </div>
-              <div class="form-check">
-                <input class="form-check-input" type="radio" name="day_of_week" id="day_of_week2" value=2>
-                <label class="form-check-label" for="gridRadios2">
-                  Tuesday
-                </label>
-              </div>
-              <div class="form-check disabled">
-                <input class="form-check-input" type="radio" name="day_of_week" id="day_of_week3" value=3>
-                <label class="form-check-label" for="gridRadios3">
-                  Wednesday
-                </label>
-              </div>
-              <div class="form-check">
-                <input class="form-check-input" type="radio" name="day_of_week" id="day_of_week4" value=4>
-                <label class="form-check-label" for="gridRadios4">
-                  Thursday
-                </label>
-              </div>
-              <div class="form-check">
-                <input class="form-check-input" type="radio" name="day_of_week" id="day_of_week5" value=5>
-                <label class="form-check-label" for="gridRadios5">
-                  Friday
-                </label>
-              </div>
-              <div class="form-check">
-                <input class="form-check-input" type="radio" name="day_of_week" id="day_of_week6" value=6>
-                <label class="form-check-label" for="gridRadios6">
-                  Saturday
-                </label>
-              </div>
-              <div class="form-check">
-                <input class="form-check-input" type="radio" name="day_of_week" id="day_of_week7" value=7>
-                <label class="form-check-label" for="gridRadios7">
-                  Sunday
-                </label>
-              </div>
-            </div>
-          </div>
-  </fieldset>
-        <input type="hidden" id="student_id" name="student_id" value="<?php echo $user_id ?>">
-        <input type="submit">
-    </div>
-    </form> 
-    </div>
-    </div>
-
-    <?php
-    if (is_array($available_session_times_data)) {
-      //if is array
-      for($i=0; $i<sizeof($available_session_times_data); $i++){
-        //running through all of the array
-        $tz = new DateTimeZone('NZ');
-        $dt = new DateTime('now',$tz);
-        $time_day = $dt->format('d'); // output: '1' - '31'
-        $time_month = $dt->format('m'); // output: '1' - '12'cc
-        $time_year = $dt->format('Y'); // output: '2023'
-        $day =  $time_year . "-" . $time_month . "-" . $time_day;
-        $name = $available_session_times_data[$i][0]; //setting name
-        $day_of_week = $available_session_times_data[$i][3]; //setting day of the week
-
-        $potential_start_time_session = $available_session_times_data[$i][1]; //setting potential start time
-        $potential_end_time_session = $available_session_times_data[$i][2]; //setting potential end time
-        $potential_starttime_rough = strtotime($day.$potential_start_time_session); //getting a time value
-        $potential_endtime_rough = strtotime($day.$potential_end_time_session); //getting a time value
-        if (date('N') == $day_of_week){ //checking if the date is the same
-          $potential_starttime = $potential_starttime_rough; //if so the times need no change
-          $potential_endtime = $potential_endtime_rough;
-
-        } elseif (date('N') > $day_of_week){ //if it is different 
-          $time_diff = date('N') - $day_of_week; //find the time difference
-          $potential_starttime = $potential_starttime_rough - ($time_diff * 86400);//accounts for the diffence
-          $potential_endtime = $potential_endtime_rough - ($time_diff * 86400);
-
-        } elseif(date('N') < $day_of_week){//if it is different
-          $time_diff = $day_of_week - date('N'); //finds the time difference 
-          $potential_starttime = $potential_starttime_rough + ($time_diff * 86400); //acounts for the difference
-          $potential_endtime = $potential_endtime_rough + ($time_diff * 86400);
-        }
-
-
-        $card_id = $available_session_times_data[$i][4];
-  
-        ?>    <div id=<?php echo $card_id; ?> class='card mx-auto' style="width: 15rem;"><?php
-        echo ($name."<br>".date("l h:i:s A", $potential_starttime) . "<br>");
-        echo date("l h:i:s A", $potential_endtime); //prints out the cards of the time sessions.
-        ?> <a href="delete_calendar_time.php?id=<?php echo $card_id; ?>">Remove</a>     </div>   <?php      }
-    }?>
-
-
-    <div id='calendar'></div>
     <script src="content.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     
   </body>
 </html>
+<div class="container">
+    <div class="main-body">
+          <div class="row gutters-sm">
+            <div class="col-md-4 mb-3">
+              <div class="card">
+                <div class="card-body">
+                  <div class="d-flex flex-column align-items-center text-center">
+                    <img src="sys_img\legacy_icon.jpg" alt="Admin" class="rounded-circle" width="150">
+                    <div class="mt-3">
+                      <h4><?php echo $display_name ?></h4>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-8">
+              <div class="card mb-3">
+                <div class="card-body">
+                  
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0">Email</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                      mine.dia.monds
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0">Phone</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                      (nyc) 9-11-2001
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0">Tutoring subjects:</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                    <?php
+                      //Get number of items in subject array
+                      if($no_tutor_subjects == false)
+                      {
+                        for($x=0;$x<sizeof($subject_array_tutor);$x++)
+                        {
+                          ?><button type="button" class="btn btn-outline-light btn-rounded btn-success"
+                          <?php echo "id=tutor_" . $subject_element_tutor_id."  ";//Give the element a unqiue id"; ?>
+                          ><?php
+                          echo $subject_array_tutor[$x][2];
+                          //Increment the id ?></button><?php
+                          $subject_element_tutor_id += 1;
+                        }
+                      }
+                    ?>
+                    <a class="btn btn-info btn-md" id=profile_edit_button>Add subjects</a>
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0">Need help with:</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                    <?php
+                      //Get number of items in subject array
+                      if($no_tutee_subjects == false)
+                      {
+                        for($x=0;$x<sizeof($subject_array_tutee);$x++)
+                        {
+                          ?><button type="button" class="btn btn-outline-light btn-rounded btn-success"
+                          <?php echo "id=tutor_" . $subject_element_tutee_id."  ";//Give the element a unqiue id"; ?>
+                          ><?php
+                          echo $subject_array_tutee[$x][2];
+                          //Increment the id ?></button><?php
+                          $subject_element_tutee_id += 1;
+                        }
+                      }
+                    ?>
+                    <a class="btn btn-info btn-md" id=profile_edit_button>Add subjects</a>
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-12">
+                      <a class="btn btn-success btn-md" id=profile_edit_button>Edit</a>
+                    </div>
+                  </div>
+                </div>
+              </div> 
+
+              </div>
+
+
+
+            </div>
+          </div>
+
+        </div>
+    </div>
+<style>
+  body{
+    margin-top:20px;
+    color: #1a202c;
+    text-align: left; 
+}
+.main-body {
+    padding: 15px;
+}
+.card {
+    box-shadow: 0 1px 3px 0 rgba(0,0,0,.1), 0 1px 2px 0 rgba(0,0,0,.06);
+}
+
+.card {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    min-width: 0;
+    word-wrap: break-word;
+    background-color: #fff;
+    background-clip: border-box;
+    border: 0 solid rgba(0,0,0,.125);
+    border-radius: .25rem;
+}
+
+.card-body {
+    flex: 1 1 auto;
+    min-height: 1px;
+    padding: 1rem;
+}
+
+.gutters-sm {
+    margin-right: -8px;
+    margin-left: -8px;
+}
+button:hover {
+  background-color: #FF2400 !important;
+  transition: 0.5s;
+}
+.gutters-sm>.col, .gutters-sm>[class*=col-] {
+    padding-right: 8px;
+    padding-left: 8px;
+}
+.mb-3, .my-3 {
+    margin-bottom: 1rem!important;
+}
+
+.bg-gray-300 {
+    background-color: #e2e8f0;
+}
+.h-100 {
+    height: 100%!important;
+}
+.shadow-none {
+    box-shadow: none!important;
+}
+</style>

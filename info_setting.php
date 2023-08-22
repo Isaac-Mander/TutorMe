b4fc2a35c404ac1546f0199934bffed4a4c6556a
Hellolightn3ss <68981667+Hellolightn3ss@users.noreply.github.com>
2023-08-22 17:34:00 +1200

    fix

--- info_setting.php
+++ info_setting.php
@@ -331,7 +331,6 @@ $tz = new DateTimeZone('NZ');
   array_multisort($array_tutor_subject_name_array_column, SORT_ASC, $array_tutor_subject_level_array_column, SORT_ASC, $subject_array_tutor);
 
   ?>
-<link rel="stylesheet" href="sys_page/styles.css">
     <h1 class="text-center">Setup page</h1>
     <p class="text-center">On this page you can set your subjects and the times in which you are free</p>
   
